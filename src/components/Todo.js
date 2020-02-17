06fa75fe8e1e0c0258b253aa36412de13359add9
Samrat Shakya <shakyasamrat@192-168-1-101.tpgi.com.au>
2020-02-17 16:12:18 +1100

    First commit

---
+++ src/components/Todo.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PropTypes from "prop-types";
+const Todo = ({ onClick, completed, text }) => (
+  <li className="list" onClick={onClick}>
+    {text}
+  </li>
+);
+Todo.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  completed: PropTypes.bool.isRequired,
+  text: PropTypes.string.isRequired
+};
+export default Todo;
